65a2a40ba953e246f0756ea32f511abf4ec3f60c
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-04 18:37:26 +0530

    Basic working setup

--- src/main/java/com/tw/SpringAOP/SpringAopApplication.java
+++ src/main/java/com/tw/SpringAOP/SpringAopApplication.java
@@ -1,13 +1,21 @@
 package com.tw.SpringAOP;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello world!");
+        };
+    }
 }
