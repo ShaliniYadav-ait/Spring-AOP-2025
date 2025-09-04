7537a49088159ed67901b2d954c2135a574ecbbc
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-04 20:50:02 +0530

    advise will match only app DAO

--- src/main/java/com/tw/SpringAOP/aspect/DemoLoggingAspect.java
+++ src/main/java/com/tw/SpringAOP/aspect/DemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class DemoLoggingAspect {
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.tw.SpringAOP.dao.AppDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("Advise running...");
     }
